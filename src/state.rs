d461b723a0dfae248c074f7c69ad1dcb7aaeda0d
Joshua Mashburn <joshualmashburn@tamu.edu>
2019-10-24 11:53:07 -0500

    added stack, keypad input, and video memory

--- src/state.rs
+++ src/state.rs
@@ -7,6 +7,9 @@ struct State{
     sound_timer: u8,    //sound timer
     delay_timer: u8,    //delay timer
     pc: u16,            //program counter
+    stack: [u16; 16],   //stack
     sp: u8,             //stack pointer
-    memory: [u8; 4096]  //4kb of memory
+    memory: [u8; 4096], //4kb of memory
+    keypad: [u8; 16],   //keypad input
+    video: [u32; 64*32] //vram
 }
